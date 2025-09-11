5ce7b7df50ad819869ab7b028d72f175efcfc22f
Baetato <diana021015@gmail.com>
2025-09-11 10:18:14 +0900

    fix: fix session code

--- public/order-system/js/config.js
+++ public/order-system/js/config.js
@@ -3,7 +3,7 @@
   var isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
   window.RUNTIME = {
     API_BASE: isLocal ? 'http://localhost:3000' : 'https://api.limswoo.shop', // 운영 도메인
-    SESSION_OPEN_CODE: 'test123',         // 운영에선 서버 설정과 맞춰야 함
+    SESSION_OPEN_CODE: 'sC2mj4Kgp',      
     USE_FIREBASE_READ: true,
     USE_FIREBASE_WRITE_MIRROR: true,
     ADMIN_PIN: '2025'
